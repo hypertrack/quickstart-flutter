14791b58979023c893f71a51560dab6025de3c94
pavel-kuznetsov-hypertrack <pavel@hypertrack.io>
2023-02-09 15:25:37 +0200

    Adapt to Transitional API
    Update Flutter SDK wrapper to 1.0.0.
    Change app name to "Quickstart Flutter"
    Set device name and metadata on init.
    Display subscription results.
    Update README.
    Add CONTRIBUTING.

---
+++ android/app/src/main/kotlin/com/hypertrack/quickstart/flutter/android/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hypertrack.quickstart.flutter.android
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,52 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:hypertrack_plugin/const/constants.dart';
+import 'package:flutter/services.dart';
+import 'package:hypertrack_plugin/data_types/json.dart';
+import 'package:hypertrack_plugin/data_types/result.dart';
 import 'package:hypertrack_plugin/hypertrack.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
+const String _publishableKey = '< Put your publishable_key here >';
 
-void main() {
+Future<void> main() async {
+  DartPluginRegistrant.ensureInitialized();
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  HyperTrack hyperTrack =
+      await HyperTrack.initialize(_publishableKey, loggingEnabled: true)
+          .onError((error, stackTrace) {
+            throw Exception(error);
+          });
+  runApp(MyApp(hyperTrack));
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final HyperTrack hyperTrack;
+
+  const MyApp(HyperTrack this.hyperTrack, {Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<MyApp> createState() => _MyAppState(this.hyperTrack);
 }
 
 class _MyAppState extends State<MyApp> {
-  HyperTrack _hypertrackFlutterPlugin = HyperTrack();
-  final String _publishableKey = "<-- PLACE PUBLIC KEY HERE -->";
-  final String _deviceName = '<-- DEVICE NAME GOES HERE -->';
-  String _result = 'Not initialized';
-  bool isRunning = false;
+  HyperTrack _hypertrackSdk;
+
+  _MyAppState(this._hypertrackSdk);
+
+  String _trackingStateText = 'Not initialized';
+  String _trackingErrorText = '';
+  String _isAvailableText = 'Not initialized';
+  String _deviceId = '';
 
   @override
   void initState() {
     super.initState();
-    Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
     initHyperTrack();
+    updateDeviceId();
+
+    _hypertrackSdk.setName(_deviceName);
+    _hypertrackSdk.setMetadata(_testMetadata);
   }
 
   @override
@@ -38,86 +56,248 @@ class _MyAppState extends State<MyApp> {
         colorScheme: const ColorScheme.light(primary: Colors.green),
       ),
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('HyperTrack Quickstart'),
-            centerTitle: true,
-          ),
-          body: ListView(
+        appBar: AppBar(
+          title: const Text('HyperTrack Quickstart'),
+          centerTitle: true,
+        ),
+        body: Builder(
+          builder: (builder) => ListView(
             children: [
               SizedBox(height: 10),
               ListTile(
                 leading: const Text("Device name"),
-                trailing: Text(
-                  _deviceName,
+                trailing: Container(
+                  width: 180,
+                  child: Text(
+                    _deviceName,
+                  ),
                 ),
+              ),
+              Container(
+                height: 65,
+                width: 400,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        enabled: false,
+                        style: TextStyle(fontSize: 13),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                        controller: TextEditingController(text: _deviceId),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        ClipboardData data = ClipboardData(text: _deviceId);
+                        await Clipboard.setData(data);
+                        print(_deviceId);
+                        _showSnackBarMessage(
+                            builder, "Device ID copied to clipboard");
+                      },
+                      child: Text("Copy"),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 50,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text("isTracking state"),
+                    ),
+                  ),
+                  Flexible(child: Text(_trackingStateText))
+                ],
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 50,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text("isAvailable state"),
+                    ),
+                  ),
+                  Flexible(child: Text(_isAvailableText.toString()))
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: 50,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: const Text("Errors"),
+                    ),
+                  ),
+                  Flexible(
+                    child: Text(
+                      _trackingErrorText,
+                    ),
+                  )
+                ],
               ),
               ButtonBar(
                 alignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
                 children: [
                   ElevatedButton(
-                    style: ElevatedButton.styleFrom(primary: isRunning ? Colors.red : Colors.green),
                     onPressed: () {
-                      isRunning
-                          ? _hypertrackFlutterPlugin.stop()
-                          : _hypertrackFlutterPlugin.start();
-                      setState(() {});
+                      _hypertrackSdk.startTracking();
                     },
-                    child:
-                    Text(isRunning ? "Stop Tracking" : "Start Tracking"),
-
-
+                    child: Text("Start Tracking"),
                   ),
                   ElevatedButton(
-                    onPressed: () async =>
-                        _hypertrackFlutterPlugin.syncDeviceSettings(),
-                    child: const Text("Sync Settings"),
+                    onPressed: () {
+                      _hypertrackSdk.stopTracking();
+                    },
+                    child: Text("Stop Tracking"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      _hypertrackSdk.setAvailability(true);
+                    },
+                    child: Text("Set Available"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      _hypertrackSdk.setAvailability(false);
+                    },
+                    child: Text("Set Unavailable"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      bool isAvailable = await _hypertrackSdk.isAvailable;
+                      _showSnackBarMessage(
+                          builder, "isAvailable: $isAvailable");
+                    },
+                    child: Text("isAvailable"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      bool isTracking = await _hypertrackSdk.isTracking;
+                      _showSnackBarMessage(builder, "isTracking: $isTracking");
+                    },
+                    child: Text("isTracking"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final result = await _hypertrackSdk.location;
+                      _showSnackBarMessage(builder, result.toString());
+                    },
+                    child: Text("Get location"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final result =
+                          await _hypertrackSdk.addGeotag(_testGeotag);
+                      if (result is Success) {
+                        _showSnackBarMessage(
+                            builder, "Geotag added at $result");
+                      } else {
+                        _showSnackBarMessage(builder, "Geotag error: $result");
+                      }
+                    },
+                    child: Text("Add geotag"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      _hypertrackSdk.sync();
+                      _showSnackBarMessage(builder, "Sync");
+                    },
+                    child: Text("Sync"),
                   ),
                 ],
               ),
             ],
           ),
-          bottomNavigationBar: ListTile(
-            leading: Text("Status"),
-            trailing: Text(_result),
-          )),
+        ),
+      ),
     );
   }
 
-  void initHyperTrack() async {
-    _hypertrackFlutterPlugin = await HyperTrack().initialize(_publishableKey);
-    _hypertrackFlutterPlugin.enableDebugLogging();
-    _hypertrackFlutterPlugin.setDeviceName(_deviceName);
-    _hypertrackFlutterPlugin.setDeviceMetadata({"source": "flutter sdk"});
-    _hypertrackFlutterPlugin.onTrackingStateChanged
-        .listen((TrackingStateChange event) {
+  void initHyperTrack() {
+    _hypertrackSdk.onTrackingChanged.listen((bool isTracking) {
       if (mounted) {
-        updateButtonState();
-        _result = getTrackingStatus(event);
+        _trackingStateText = "Tracking: $isTracking";
+        _trackingErrorText = "";
+        setState(() {});
+      }
+    }).onError((error) {
+      if (mounted) {
+        _trackingStateText = error.toString();
+        setState(() {});
+      }
+    });
+    _hypertrackSdk.onAvailabilityChanged.listen((bool isAvailable) {
+      if (mounted) {
+        _isAvailableText = "Available: ${isAvailable}";
+        _trackingErrorText = "";
+        setState(() {});
+      }
+    }).onError((error) {
+      if (mounted) {
+        _isAvailableText = error.toString();
+        setState(() {});
+      }
+    });
+    _hypertrackSdk.onError.listen((errors) {
+      if (mounted) {
+        _trackingErrorText =
+            errors.map((e) => {e.toString().split('.').last}).join("\n");
+        setState(() {});
+      }
+    }).onError((error) {
+      if (mounted) {
+        _trackingErrorText = error.toString();
         setState(() {});
       }
     });
   }
 
-  void updateButtonState() async {
-    final temp = await _hypertrackFlutterPlugin.isRunning();
-    isRunning = temp;
+  void updateDeviceId() async {
+    _deviceId = await _hypertrackSdk.deviceId;
     setState(() {});
+  }
+
+  void _showSnackBarMessage(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        duration: Duration(milliseconds: 1000),
+        content: RichText(
+          text: TextSpan(
+            children: [message]
+                .map<TextSpan>(
+                  (String message) => TextSpan(text: message),
+                )
+                .toList(),
+          ),
+        )));
   }
 }
 
-String getTrackingStatus (TrackingStateChange event) {
-  Map<TrackingStateChange, String> statusMap = {
-    TrackingStateChange.start: "Tracking Started",
-    TrackingStateChange.stop: "Tracking Stop",
-    TrackingStateChange.unknownError: "Unknown Error",
-    TrackingStateChange.authError: "Authentication Error",
-    TrackingStateChange.networkError: "Network Error",
-    TrackingStateChange.invalidToken: "Invalid Token",
-    TrackingStateChange.locationDisabled: "Location Disabled",
-    TrackingStateChange.permissionsDenied: "Permissions Denied",
-  };
-  if (statusMap[event] == null) {
-    throw Exception("Unexpected null value in getTrackingStatus");
-  }
-  return statusMap[event]!;
-}
+const String _deviceName = 'Flutter Quickstart';
+final JSONObject _testMetadata = JSONObject({
+  "source": JSONString("Flutter"),
+  "metadata": JSONObject({"value": JSONNumber(Random().nextDouble())})
+});
+final JSONObject _testGeotag = JSONObject({
+  "source": JSONString("Flutter"),
+  "payload": JSONObject({"test_payload": JSONNumber(1)})
+});
